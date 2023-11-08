dfa8d5ce34152e47c2ab90202db5f5be5e1c8d7b
rupasreevalli <rupasreevalli1@gmail.com>
2023-11-08 16:36:41 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def jampandu(request):
+    return <h1><marquee>HttpResponse</h1><marquee>('Hai jampand')    
+
+# Create your views here.
